07102c541ab7a8b82dfd8e4278a7d0efe7a2dc4f
Davoud Ataee Tarzanagh <tarzanagh@gmail.com>
2025-03-13 18:35:57 -0700

    Update savenetwork.py

--- savenetwork.py
+++ savenetwork.py
@@ -1,13 +1,4 @@
-import networkx as nx
-
-# Save as GEXF (Graph Exchange XML Format)
-nx.write_gexf(enhanced_graph, "enhanced_graph.gexf")
-
-# Alternative formats:
-# GRAPHML
-nx.write_graphml(enhanced_graph, "enhanced_graph.graphml")
-
-# JSON
-nx.write_json(enhanced_graph, "enhanced_graph.json")
-
-# GEXF preserves all node and edge attributes and is compatible with visualization tools like Gephi
+import json
+with open("enhanced_graph.json", "w") as f:
+    json_graph = nx.node_link_data(enhanced_graph)
+    json.dump(json_graph, f)
