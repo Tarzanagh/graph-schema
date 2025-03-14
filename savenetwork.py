308dc601349bbe94007f145fa0f55fbae9dc0482
Davoud Ataee Tarzanagh <tarzanagh@gmail.com>
2025-03-13 18:52:04 -0700

    Update savenetwork.py

--- savenetwork.py
+++ savenetwork.py
@@ -1,2 +1,18 @@
-import json
-json.dump(nx.node_link_data(enhanced_graph), open("enhanced_graph.json", "w"))
+# Save enhanced graph to file
+def save_enhanced_graph(graph, filename):
+    """Simple function to save a NetworkX graph to file"""
+    import networkx as nx
+    import pickle
+    
+    # Save using pickle (preserves all attributes)
+    with open(filename, 'wb') as f:
+        pickle.dump(graph, f)
+    
+    print(f"Graph saved to {filename}")
+    
+    # To load later:
+    # with open(filename, 'rb') as f:
+    #     loaded_graph = pickle.load(f)
+
+# Usage:
+save_enhanced_graph(enhanced_graph, "enhanced_graph.pkl")
